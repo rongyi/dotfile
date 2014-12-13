c95c178eb6fd025b2413a084e55d8103986f06fb
rongyi <rongyi@ucloud.cn>
2014-12-13 12:49:56 +0800

    update vim config

--- vim/deploy.sh
+++ vim/deploy.sh
@@ -4,8 +4,6 @@
 mkdir -p $HOME/.vim/{bundle,autoload}
 
 #get pathogen
-#you may need this when you have a fucking working environment like me.
-#export http_proxy=http://192.168.X.XX:XXXX
 sudo apt-get -y install curl
 curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim
 #get repo
