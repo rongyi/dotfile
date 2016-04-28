1092d899a3efedd6cfe58e759ba4be6f23e44fff
rongyi <TODO>
2016-04-28 09:27:44 +0800

    upda

---
+++ sh/bootstrap.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# for dev
+apt-get install git build-essential emacs libx11-dev libxaw7-dev libjpeg8-dev libpng++-dev libgif-dev libtiff5-dev libtinfo-dev libncurses5-dev libgnutls-dev libpcre3-dev xaw3dg-dev gnome-terminal zsh
+
+# for vim
