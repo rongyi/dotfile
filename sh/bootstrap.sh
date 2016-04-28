9a062b5f522df94ccba908c501aacdb98e594fbb
rongyi <sqrongyi@163.com>
2016-04-28 13:37:22 +0800

    Update bootstrap

--- sh/bootstrap.sh
+++ sh/bootstrap.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 # for dev
-apt-get install git build-essential emacs libx11-dev libxaw7-dev libjpeg8-dev libpng++-dev libgif-dev libtiff5-dev libtinfo-dev libncurses5-dev libgnutls-dev libpcre3-dev xaw3dg-dev gnome-terminal zsh
-
-# for vim
+sudo apt-get install build-essential emacs git gnome-terminal libatk1.0-dev libbonoboui2-dev libcairo2-dev libcurl3 libcurl4-openssl-dev libexpat1-dev libexpat1-dev libgif-dev libgnome2-dev libgnomeui-dev libgnutls-dev libgtk2.0-dev libjpeg8-dev libncurses5-dev libpcre3-dev libpng++-dev libssh-dev libtiff5-dev libx11-dev libxaw7-dev libxpm-dev libxt-dev openssh-server python-dev ruby-dev xaw3dg-dev zsh
