33134b398063a91e9339dc1716ae9e0c10927690
rongyi <sqrongyi@163.com>
2016-04-29 14:43:14 +0800

    Update bootstrap script

--- sh/bootstrap.sh
+++ sh/bootstrap.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # packages for dev
-sudo apt-get install build-essential emacs git gnome-terminal libatk1.0-dev libbonoboui2-dev libcairo2-dev libcurl3 libcurl4-openssl-dev libexpat1-dev libexpat1-dev libgif-dev libgnome2-dev libgnomeui-dev libgnutls-dev libgtk2.0-dev libjpeg8-dev libncurses5-dev libpcre3-dev libpng++-dev libssh-dev libtiff5-dev libx11-dev libxaw7-dev libxpm-dev libxt-dev openssh-server python-dev ruby-dev xaw3dg-dev zsh exuberant-ctags libtool automake python-pip cmake
+sudo apt-get install build-essential emacs git gnome-terminal libatk1.0-dev libbonoboui2-dev libcairo2-dev libcurl3 libcurl4-openssl-dev libexpat1-dev libexpat1-dev libgif-dev libgnome2-dev libgnomeui-dev libgnutls-dev libgtk2.0-dev libjpeg8-dev libncurses5-dev libpcre3-dev libpng++-dev libssh-dev libtiff5-dev libx11-dev libxaw7-dev libxpm-dev libxt-dev openssh-server python-dev ruby-dev xaw3dg-dev zsh exuberant-ctags libtool automake python-pip cmake libevent-dev
 
 # build vim
 cd ~
