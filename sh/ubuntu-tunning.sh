aa23bd175cdd75c69b62fff218399a8b2a94e79c
rongyi <sqrongyi@163.com>
2016-05-25 13:37:43 +0800

    Update ubuntu tunning script

--- sh/ubuntu-tunning.sh
+++ sh/ubuntu-tunning.sh
@@ -2,3 +2,6 @@
 
 # disable super
 dconf write /org/compiz/profiles/unity/plugins/unityshell/show-launcher '""'
+
+# change alt-tab to my habit
+sudo apt-get install -y compiz-plugins compiz-plugins-extra compizconfig-settings-manager
