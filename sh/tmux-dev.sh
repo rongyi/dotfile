3b5a8f8b265ce989b7f622c23640a9dc43fe4a6a
rongyi <sqrongyi@163.com>
2016-10-01 10:17:47 +0800

    Delete useless shell

--- sh/tmux-dev.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/zsh
-tmux new-session -d
-tmux split-window -h
-tmux new-window "ssh rydev"
-tmux new-window
-tmux new-window
-tmux new-window "cd /home/ry/message/proto "
-tmux -2 attach-session -d
