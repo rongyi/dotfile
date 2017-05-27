f3c6e4b81f4d9c01e12fb7f538622df3e45ecb84
rongyi <TODO>
2017-05-27 17:10:22 +0800

    Update dotfile

--- sh/bootstrap-mac.sh
+++ sh/bootstrap-mac.sh
@@ -5,3 +5,6 @@ chsh -s $(which zsh)
 
 # copy content from this link
 https://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in-mac-os-x/
+
+#workaround for ycmd restart
+https://github.com/Valloric/ycmd/issues/126
