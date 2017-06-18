7fa98e836b941ef9401edbb5a7c41537e03f4c98
rongyi <sqrongyi@163.com>
2017-06-18 16:23:35 +0800

    Update emacs section for macos

--- sh/bootstrap-mac.sh
+++ sh/bootstrap-mac.sh
@@ -8,3 +8,20 @@ https://www.topbug.net/blog/2013/04/14/install-and-use-gnu-command-line-tools-in
 
 #workaround for ycmd restart
 https://github.com/Valloric/ycmd/issues/126
+
+
+# install emacs for mac, other way emacs kind of slow, this is the fastest I found
+# https://github.com/howardabrams/dot-files/blob/master/emacs.org
+brew cask install xquartz
+
+brew edit emacs
+
+# And add the following section
+
+patch do
+  url "https://github.com/minimal/emacs/commit/812dd5119645a09bc025a9dddedad9474d12ecb6.diff"
+end
+
+brew install emacs --HEAD --use-git-head --with-cocoa --with-gnutls --with-librsvg --with-ns --with-imagemagick
+
+brew linkapps emacs
