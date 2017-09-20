405f3dbd59e9134e76bdcb0492547cec5d00832a
rongyi <sqrongyi@163.com>
2017-09-20 10:51:28 +0800

    Update go bootstrap shell

--- sh/bootstrap-go.sh
+++ sh/bootstrap-go.sh
@@ -2,3 +2,4 @@ go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
 go get -u -v golang.org/x/tools/cmd/guru
 go get -u github.com/golang/lint/golint
+go get -v github.com/zmb3/gogetdoc
