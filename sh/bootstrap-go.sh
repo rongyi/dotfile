d3b133c35fd5c4723afd1d135849b164326eacad
rongyi <sqrongyi@163.com>
2017-09-26 08:40:04 +0800

    Upate go bootstrap script

--- sh/bootstrap-go.sh
+++ sh/bootstrap-go.sh
@@ -3,3 +3,6 @@ go get -u -v github.com/rogpeppe/godef
 go get -u -v golang.org/x/tools/cmd/guru
 go get -u github.com/golang/lint/golint
 go get -v github.com/zmb3/gogetdoc
+# linter
+go get -u gopkg.in/alecthomas/gometalinter.v1
+gometalinter.v1 --install --update
