0246481c6a581ff29feeb2e0434401c0af2f7f4e
rongyi <sqrongyi@163.com>
2017-09-29 14:57:49 +0800

    Update go bootstrap script

--- sh/bootstrap-go.sh
+++ sh/bootstrap-go.sh
@@ -1,8 +1,16 @@
+#!/bin/bash
+
+#editor related
 go get -u -v github.com/nsf/gocode
 go get -u -v github.com/rogpeppe/godef
 go get -u -v golang.org/x/tools/cmd/guru
 go get -u github.com/golang/lint/golint
 go get -v github.com/zmb3/gogetdoc
-# linter
-go get -u gopkg.in/alecthomas/gometalinter.v1
-gometalinter.v1 --install --update
+# linter, make editor too slow
+# go get -u gopkg.in/alecthomas/gometalinter.v1
+# gometalinter.v1 --install --update
+
+# profile
+go get -v github.com/uber/go-torch
+# request tool
+go get -v github.com/rakyll/hey
