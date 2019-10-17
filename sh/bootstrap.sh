b300584b6517d037e16352e8f4db039b697e1df9
rongyi <sqrongyi@163.com>
2019-10-17 19:41:11 +0800

    Update dotfile

--- sh/bootstrap.sh
+++ sh/bootstrap.sh
@@ -4,7 +4,7 @@
 # execute the script as root please
 
 # packages for dev
-sudo apt-get install -y build-essential git gnome-terminal libatk1.0-dev libbonoboui2-dev libcairo2-dev libcurl3 libcurl4-openssl-dev libexpat1-dev libexpat1-dev libgif-dev libgnome2-dev libgnomeui-dev libgnutls-dev libgtk2.0-dev libjpeg8-dev libncurses5-dev libpcre3-dev libpng++-dev libssh-dev libtiff5-dev libx11-dev libxaw7-dev libxpm-dev libxt-dev openssh-server python-dev ruby-dev xaw3dg-dev zsh exuberant-ctags libtool automake python-pip cmake libevent-dev vim tmux curl unrar
+sudo apt-get install -y build-essential git gnome-terminal libatk1.0-dev libbonoboui2-dev libcairo2-dev libcurl4 libcurl4-openssl-dev libexpat1-dev libexpat1-dev libgif-dev libgnome2-dev libgnomeui-dev libgnutls28-dev libgtk2.0-dev libjpeg8-dev libncurses5-dev libpcre3-dev libpng++-dev libssh-dev libtiff5-dev libx11-dev libxaw7-dev libxpm-dev libxt-dev openssh-server python-dev ruby-dev xaw3dg-dev zsh exuberant-ctags libtool automake python-pip cmake libevent-dev vim tmux curl unrar clang-format
 
 # for Lisp doc
 sudo apt-get install -y hyper-spec sbcl sbcl-source sbcl-doc clisp
